cadefd580864917eb0a434b2b307b612b554fcde
jaisriv788 <srivastavajai845@gmail.com>
2023-04-23 21:36:01 +0530

    react app deployment

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(URL, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        "x-rapidapi-key": "a4d9c7e24fmshd292020014cbd81p1367ddjsn27a9e05a6aec",
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
